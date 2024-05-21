d8bacb537c662e97f3e89c42f37b91553a748821
vanh <vanhbeo1912@gmail.com>
2024-05-21 10:34:36 +0700

    first

---
+++ app/src/main/java/com/example/btth4/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.btth4;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtMaLop, edtTenLop, edtSiSo;
+    Button btnInsert, btnDelete, btnUpdate;
+    ListView lv;
+    ArrayList<String> myList;
+    ArrayAdapter<String> myAdapter;
+    SQLiteDatabase myDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        edtMaLop = findViewById(R.id.edtMaLop);
+        edtTenLop = findViewById(R.id.edtTenLop);
+        edtSiSo = findViewById(R.id.edtSiSo);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        lv = findViewById(R.id.lv);
+        myList = new ArrayList<>();
+        myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myList);
+        lv.setAdapter(myAdapter);
+        myDatabase = openOrCreateDatabase("qlSinhVien.db", MODE_PRIVATE, null);
+        try {
+            String sql = "Create table tbllop(malop TEXT primary key, tenlop TEXT, siso INTEGER)";
+            myDatabase.execSQL(sql);
+        } catch (Exception e) {
+            Log.e("Error", "Table da ton tai");
+        }
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtMaLop.getText().toString();
+                String tenlop = edtTenLop.getText().toString();
+                int siso = Integer.parseInt(edtSiSo.getText().toString());
+                ContentValues myValue = new ContentValues();
+                myValue.put("malop", malop);
+                myValue.put("tenlop", tenlop);
+                myValue.put("siso", siso);
+                String msg = "";
+                if(myDatabase.insert("tbllop", null, myValue) == -1) {
+                    msg = "Failed to insert record!";
+                } else {
+                    msg = "Insert record successfully!";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtMaLop.getText().toString();
+                int n = myDatabase.delete("tbllop", "malop = ?", new String[]{malop});
+            }
+        });
+    }
+}
